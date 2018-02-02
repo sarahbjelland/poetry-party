5686c0493d05e52934091ddb034970505bbc2d84
Sarah Bjelland <sarahbjelland@me.com>
2018-02-02 11:03:54 -0600

    Adding name comments to shell script for verbosity

--- wordcount.sh
+++ wordcount.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# get rid of comment lines
+# search for word (case insensitive, whole words only)
+# translate spaces to new lines
+# search for word (case insensitive, whole words only)
+# word count by line to get total number of occurances
 grep -v "^#$2 |grep -i -w $1 | tr ' ' '\n' | grep -i -w $1 | wc -l
